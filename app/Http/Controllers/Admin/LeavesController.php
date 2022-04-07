b1ef1860e42b33fc76aefb0abfb8c3082f3c9e29
RedouaneJnah98 <redouanejnah2000@gmail.com>
2022-04-07 14:51:36 +0000

    Changed to Resource controller

---
+++ app/Http/Controllers/Admin/LeavesController.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use App\Models\LeaveApplication;
+use Illuminate\Http\Request;
+
+class LeavesController extends Controller
+{
+    public function index()
+    {
+        $leave_applications = LeaveApplication::paginate(15);
+
+        return view('admin.leaves.index', compact('leave_applications'));
+    }
+
+    public function create()
+    {
+        //
+    }
+
+    public function store(Request $request)
+    {
+        //
+    }
+
+    public function show($id)
+    {
+        //
+    }
+
+    public function edit($id)
+    {
+        //
+    }
+
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    public function destroy($id)
+    {
+        //
+    }
+}
